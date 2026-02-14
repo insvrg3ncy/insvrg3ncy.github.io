2c15b437b46043c5dbc4251ff8b877e6a055ddfd
iAmScienceMan <63004048+iAmScienceMan@users.noreply.github.com>
2026-02-14 23:54:58 +0300

    переделал сайт, пофиксил баги, добавил фавики и бейджи

--- config.js
+++ config.js
@@ -33,7 +33,8 @@ window.SITE_CONFIG = {
   },
 
   socials: [
-    { name: "Discord", url: "https://discord.com/HSC6Frb6ma", label: "HSC6Frb6ma" },
+    { name: "Discord", url: "https://discord.com/users/1043834316620304394", label: "destroysquad" },
+    { name: "Discord Server", url: "https://discord.gg/HSC6Frb6ma", label: "HSC6Frb6ma"},
     { name: "YouTube", url: "https://youtube.com/@moridolbaeb", label: "@moridolbaeb" },
     { name: "Telegram", url: "https://t.me/codingsession", label: "codingsession" },
     { name: "Steam", url: "https://steamcommunity.com/id/insvrg3ncy", label: "insvrg3ncy" },
