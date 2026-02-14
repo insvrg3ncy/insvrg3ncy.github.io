611b3244c8d6dfbfe0a998888a93f32133aff056
insvrg3ncy <gunsshow9@gmail.com>
2026-02-14 20:49:08 +0300

    first commit

---
+++ character.js
@@ -0,0 +1,3 @@
+const character = document.getElementById("character");
+character.src = "1.png";
+character.style.setProperty("--char-scale", "1.5");
---
+++ config.js
@@ -0,0 +1,41 @@
+window.SITE_CONFIG = {
+  personal: {
+    title: "Personal",
+    subtitle: "",
+    paragraphs: [
+      "Hello, i'm Mori, a 1x y.o. Space Station 14 developer from russia. I've got my own project there but in WIP state",
+      "Mainly i code in python and CSharp",
+      "Now I'm taking orders for Telegram bots and apps",
+      "If you want to contact me, you can do it via discord or telegram.",
+    ],
+  },
+
+  games: {
+    title: "My games",
+    subtitle: "",
+    paragraphs: [
+      "Gmod",
+      "Counter Strike 2",
+      "Space Station 14",
+      "BeamNG.drive",
+      "Vtol VR",
+      "Payday 2",
+      "Apex Legends (Retired player)",
+    ],
+  },
+
+  videos: {
+    title: "Videos",
+    subtitle: "",
+    paragraphs: [
+      "My YouTube channel: https://www.youtube.com/@moridolbaeb",
+    ],
+  },
+
+  socials: [
+    { name: "Discord", url: "https://discord.com/HSC6Frb6ma", label: "HSC6Frb6ma" },
+    { name: "YouTube", url: "https://youtube.com/@moridolbaeb", label: "@moridolbaeb" },
+    { name: "Telegram", url: "https://t.me/codingsession", label: "codingsession" },
+    { name: "Steam", url: "https://steamcommunity.com/id/insvrg3ncy", label: "insvrg3ncy" },
+  ],
+};
